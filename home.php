b375d5da981936e088a00f1a6fee5728803a3b0d
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-08-16 15:31:50 +0800

    :truck: Move generic styles, and media queries out of the css file for nav bar

--- home.php
+++ home.php
@@ -6,7 +6,9 @@
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/all.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<link rel="stylesheet" type="text/css" href="css/vertical-nav-bar.css" />
+	<link rel="stylesheet" type="text/css" href="css/media-queries.css" />
 	<title></title>
 </head>
 <body>
