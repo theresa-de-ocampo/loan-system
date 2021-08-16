e680a08a246d16fe347f00885904e82fca386caa
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-08-16 14:47:33 +0800

    :wrench: Add location configuration

---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+define("COOPERATIVE", "Ciudad Nuevo");
+define("SUBDIVISION", "Ciudad Nuevo");
+define("BARANGAY", "Timalan Balsahan");
+define("CITY_TOWN", "Naic");
+define("PROVINCE", "Cavite");
