1981967ab3ad59cf44c61cba6aee74e7aff9ec60
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-09-29 13:29:29 +0800

    :card_file_box: Add second loan scenario

--- config/setup.sql
+++ config/setup.sql
@@ -896,6 +896,7 @@ VALUES
 INSERT INTO 
 	`data_subject`
 VALUES
+	-- Guarantors for 2020
 	(DEFAULT, 'Jovyln', 'Caturay', 'Busque', '09490394823', '1981-04-28', 'P1, B1, L1'),
 	(DEFAULT, 'Allan', 'Angue', 'Busque', '09491128936', '1981-10-13', 'P1, B1, L1'),
 	(DEFAULT, 'Amy', 'Angue', 'Cahu', '09079038153', '1995-02-06', 'P1, B1, L2'),
@@ -904,21 +905,24 @@ VALUES
 	(DEFAULT, 'Raquel', 'Pagtakhan', 'Loyola', '09184720087', '1980-05-25', 'P1, B1, L5'),
 	(DEFAULT, 'Iconarclo', 'Cabrera', 'Api', '09497266639', '1969-10-09', 'P1, B1, L6'),
 	(DEFAULT, 'Felicita', 'Magtibay', 'Imbis', '09183719442', '1956-06-01', 'P1, B1, L7'),
-	(DEFAULT, 'Aurora', 'Imbis', 'Liberato', '09358314949', '1984-01-01', 'P1, B2, L1'),
-	(DEFAULT, 'Beth', 'Bigalbal', 'Navalta', '09189218835', '1965-07-19', 'P1, B2, L2'),
+	-- Guarantors for 2021
+	(DEFAULT, 'Aurora', 'Imbis', 'Liberato', '09358314949', '1990-01-01', 'P1, B3, L1'),
+	(DEFAULT, 'Beth', 'Bigalbal', 'Navalta', '09189218835', '1965-07-19', 'P1, B3, L2'),
 	(DEFAULT, 'Theresa', 'Gumapas', 'De Ocampo', '09078466964', '1999-11-07', 'P2, B1, L1'),
-	(DEFAULT, 'Felicita', 'Pabiton', 'Nable', '09186475411', '1995-06-01', 'P1, B2, L3'),
-	(DEFAULT, 'Gina', 'Medina', 'Robiso', '09452579778', '1999-11-13', 'P1, B2, L4'),
-	(DEFAULT, 'Helen', 'Balatico', 'Tailon', '09072914753', '1980-11-05', 'P1, B2, L5'),
-	(DEFAULT, 'Jane', 'Hera', 'Histo', '09229013858', '1983-03-20', 'P1, B2, L6'),
-	(DEFAULT, 'Adrian', 'Ilag', 'Dela Torre', '09239174896', '1980-06-09', 'P1, B2, L7'),
-	(DEFAULT, 'Lyn', 'Lopez', 'Ledesma', '09348918364', '1994-08-23', 'P1, B2, L8'),
-	(DEFAULT, 'Mary', 'Rodriguez', 'Pingol', '09251388491', '1973-10-09', 'P1, B2, L9'),
-	(DEFAULT, 'Mavic', 'Ferrer', 'Gariando', '09071213894', '1995-01-23', 'P1, B2, L10'),
-	(DEFAULT, 'Mona', 'Nacalaban', 'Aguinaldo', '09749712038', '1956-08-23', 'P1, B2, L11'),
-	(DEFAULT, 'Nelly', 'Años', 'Zamora', '09449293018', '1991-05-18', 'P1, B2, L12'),
-	(DEFAULT, 'Nelsie', 'Bigalbal', 'Nasol', '09458129443', '1965-07-19', 'P1, B2, L13'),
-	(DEFAULT, 'Cherryluz', 'Casaul', 'Javier', '09749481225', '1981-03-21', 'P2, B1, L1');
+	(DEFAULT, 'Felicita', 'Pabiton', 'Nable', '09186475411', '1995-06-01', 'P1, B3, L3'),
+	(DEFAULT, 'Gina', 'Medina', 'Robiso', '09452579778', '1999-11-13', 'P1, B3, L4'),
+	(DEFAULT, 'Helen', 'Balatico', 'Tailon', '09072914753', '1980-11-05', 'P1, B3, L5'),
+	(DEFAULT, 'Jane', 'Hera', 'Histo', '09229013858', '1983-03-20', 'P1, B3, L6'),
+	(DEFAULT, 'Adrian', 'Ilag', 'Dela Torre', '09239174896', '1980-06-09', 'P1, B3, L7'),
+	(DEFAULT, 'Lyn', 'Lopez', 'Ledesma', '09348918364', '1994-08-23', 'P1, B3, L8'),
+	(DEFAULT, 'Mary', 'Rodriguez', 'Pingol', '09251388491', '1973-10-09', 'P1, B3, L9'),
+	(DEFAULT, 'Mavic', 'Ferrer', 'Gariando', '09071213894', '1995-01-23', 'P1, B3, L10'),
+	(DEFAULT, 'Mona', 'Nacalaban', 'Aguinaldo', '09749712038', '1956-08-23', 'P1, B3, L11'),
+	(DEFAULT, 'Nelly', 'Años', 'Zamora', '09449293018', '1991-05-18', 'P1, B3, L12'),
+	(DEFAULT, 'Nelsie', 'Bigalbal', 'Nasol', '09458129443', '1965-07-19', 'P1, B3, L13'),
+	-- Borrowers for 2021
+	(DEFAULT, 'Cherryluz', 'Casaul', 'Javier', '09749481225', '1981-03-21', 'P1, B4, L1'),
+	(DEFAULT, 'Arkin', 'Diaz', 'Hicban', '09269866738', '1984-05-27', 'P1, B4, L2');
 
 INSERT INTO
 	`guarantor`
@@ -963,46 +967,63 @@ INSERT INTO
 	`loan`
 VALUES
 	(DEFAULT, 23, 9, '2021-02-10 10:00:00', 5000, 'Closed'),
-	(DEFAULT, 22, 21, '2021-06-21 11:00:00', 3000, 'Active');
+	(DEFAULT, 24, 10, '2021-06-21 11:00:00', 25000, 'Closed');
 
 INSERT INTO
 	`principal_payment`
 VALUES
 	(DEFAULT, 3000, '2021-06-23 08:00:00', 1),
-	(DEFAULT, 2000, '2021-06-20 08:00:00', 1);
+	(DEFAULT, 2000, '2021-08-10 08:00:00', 1),
+	(DEFAULT, 10000, '2021-07-18 09:00:00', 2),
+	(DEFAULT, 15000, '2021-08-18 09:00:00', 2);
 
 INSERT INTO
 	`interest`
 VALUES
-	(DEFAULT, '2021-02-10', 500, 'Paid', 1),
-	(DEFAULT, '2021-03-10', 500, 'Paid', 1),
-	(DEFAULT, '2021-04-10', 500, 'Paid', 1),
-	(DEFAULT, '2021-05-10', 500, 'Paid', 1),
-	(DEFAULT, '2021-06-10', 500, 'Late', 1),
-	(DEFAULT, '2021-07-10', 200, 'Paid', 1),
-	(DEFAULT, '2021-08-10', 200, 'Paid', 1);
+	(DEFAULT, '2021-02-10', 500, 'Paid', 1),  -- 1
+	(DEFAULT, '2021-03-10', 500, 'Paid', 1),  -- 2
+	(DEFAULT, '2021-03-16', 2500, 'Paid', 2), -- 3
+	(DEFAULT, '2021-04-10', 500, 'Paid', 1),  -- 4
+	(DEFAULT, '2021-04-16', 2500, 'Paid', 2), -- 5
+	(DEFAULT, '2021-05-10', 500, 'Paid', 1),  -- 6
+	(DEFAULT, '2021-05-16', 2500, 'Paid', 2), -- 7
+	(DEFAULT, '2021-06-10', 500, 'Late', 1),  -- 8
+	(DEFAULT, '2021-06-16', 2500, 'Late', 2), -- 9
+	(DEFAULT, '2021-07-10', 200, 'Paid', 1),  -- 10
+	(DEFAULT, '2021-07-16', 2500, 'Paid', 2), -- 11
+	(DEFAULT, '2021-08-10', 200, 'Paid', 1),  -- 12
+	(DEFAULT, '2021-08-16', 1500, 'Paid', 2); -- 13
 
 INSERT INTO
 	`interest_payment`
 VALUES
 	(DEFAULT, 500, '2021-02-10 08:00:00', 1),
 	(DEFAULT, 500, '2021-03-10 08:00:00', 2),
-	(DEFAULT, 500, '2021-04-10 08:00:00', 3),
-	(DEFAULT, 500, '2021-05-10 08:00:00', 4),
-	(DEFAULT, 200, '2021-07-10 08:00:00', 6),
-	(DEFAULT, 200, '2021-08-10 08:00:00', 7),
-	(DEFAULT, 500, '2021-08-16 08:00:00', 5);
+	(DEFAULT, 2500, '2021-03-16 09:01:00', 3),
+	(DEFAULT, 500, '2021-04-10 08:00:00', 4),
+	(DEFAULT, 2500, '2021-04-15 09:01:00', 5),
+	(DEFAULT, 500, '2021-05-10 08:00:00', 6),
+	(DEFAULT, 2500, '2021-05-16 09:01:00', 7),
+	(DEFAULT, 1000, '2021-06-16 09:00:00', 9),
+	(DEFAULT, 1500, '2021-06-18 09:00:00', 9),
+	(DEFAULT, 200, '2021-07-10 08:00:00', 10),
+	(DEFAULT, 2500, '2021-07-16 09:01:00', 11),
+	(DEFAULT, 200, '2021-08-10 08:00:00', 12),
+	(DEFAULT, 500, '2021-08-16 08:00:00', 8),
+	(DEFAULT, 1500, '2021-08-18 09:01:00', 13);
 
 INSERT INTO
 	`penalty`
 VALUES
-	(DEFAULT, '2021-06-11', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-12', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-13', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-14', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-15', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-16', 17, 'Paid', 5, 1),
-	(DEFAULT, '2021-06-17', 500, 'Paid', 5, 1);
+	(DEFAULT, '2021-06-11', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-12', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-13', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-14', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-15', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-16', 17, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-17', 500, 'Paid', 8, 1),
+	(DEFAULT, '2021-06-17', 50, 'Paid', 9, 2),
+	(DEFAULT, '2021-06-18', 50, 'Paid', 9, 2);
 
 INSERT INTO
 	`penalty_payment`
@@ -1013,18 +1034,24 @@ VALUES
 	(DEFAULT, 17, '2021-08-16 08:04:00', 4),
 	(DEFAULT, 17, '2021-08-16 08:05:00', 5),
 	(DEFAULT, 17, '2021-08-16 08:06:00', 6),
-	(DEFAULT, 500, '2021-08-16 08:07:00', 7);
+	(DEFAULT, 500, '2021-08-16 08:07:00', 7),
+	(DEFAULT, 50, '2021-06-18 09:00:00', 8),
+	(DEFAULT, 50, '2021-06-18 09:01:00', 9);
 
 INSERT INTO
 	`processing_fee`
 VALUES
-	(DEFAULT, '2021-02-10', 60, 'Paid', 1),
-	(DEFAULT, '2021-05-10', 60, 'Paid', 1),
-	(DEFAULT, '2021-08-10', 30, 'Paid', 1);
+	(DEFAULT, '2021-02-10', 60, 'Paid', 1),  -- 1
+	(DEFAULT, '2021-03-16', 260, 'Paid', 2), -- 2
+	(DEFAULT, '2021-05-10', 60, 'Paid', 1),  -- 3
+	(DEFAULT, '2021-06-16', 260, 'Paid', 2), -- 4
+	(DEFAULT, '2021-08-10', 30, 'Paid', 1);  -- 5
 
 INSERT INTO
 	`processing_fee_payment`
 VALUES
 	(DEFAULT, 60, '2021-02-10 08:02:00', 1),
-	(DEFAULT, 60, '2021-05-10 08:02:00', 2),
-	(DEFAULT, 30, '2021-08-10 08:02:00', 3);
+	(DEFAULT, 260, '2021-03-16 09:02:00', 2),
+	(DEFAULT, 60, '2021-05-10 08:02:00', 3),
+	(DEFAULT, 260, '2021-06-16 09:02:00', 4),
+	(DEFAULT, 30, '2021-08-10 08:02:00', 5);
