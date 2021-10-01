07b13d14cfdf01c07f81d7662c87a557217f1829
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-10-01 14:02:41 +0800

    :heavy_minus_sign: Remove validation.js

--- index.php
+++ index.php
@@ -39,7 +39,6 @@
 	</div><!-- login-container -->
 
 	<script src="js/jquery-3.6.0.min.js"></script>
-	<script src="js/validation.js"></script>
 	<script>
 		$("#show-password").on("click", toggleVisibility);
 	</script>
