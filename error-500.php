acd2efd40468c79316c55b68a01f4043dd82cfc0
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-10-06 12:23:45 +0800

    :lipstick: Design generic error page

--- error-500.php
+++ error-500.php
@@ -5,9 +5,13 @@
 	<meta name="author" content="Theresa De Ocampo" />
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/error.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/others/favicon.png" />
 	<title></title>
 </head>
 <body>
+	<img src="img/others/bug.svg" alt="Fixing Graphic" />
 	<h1>Sorry, an unexpected error occurred.</h1>
+	<p>We'll check on the error report to get it up and running as soon as possible.</p>
 </body>
 </html>
