f734df598258790141c7253cca6f852acb40a093
theresa-de-ocampo <ma_theresa7@yahoo.com>
2021-10-06 15:36:43 +0800

    :heavy_plus_sign: Add cycle model

--- src/add-guarantor.php
+++ src/add-guarantor.php
@@ -3,6 +3,7 @@ if (isset($_POST["submit"])) {
 	unset($_POST["submit"]);
 	require_once "../config/config.php";
 	require_once "../lib/database-handler.php";
+	require_once "../models/Cycle.php";
 	require_once "../models/Guarantor.php";
 	$guarantor = new Guarantor();
 	$guarantor->addGuarantor($_POST);
